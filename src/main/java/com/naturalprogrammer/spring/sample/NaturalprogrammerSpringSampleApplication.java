21e7bf92f64dcbaaf383a1e342c482f4f47e7a92
Sanjay <skpatel20@gmail.com>
2015-01-05 07:25:06 +0530

    Created a new project

---
+++ src/main/java/com/naturalprogrammer/spring/sample/NaturalprogrammerSpringSampleApplication.java
@@ -0,0 +1,16 @@
+package com.naturalprogrammer.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class NaturalprogrammerSpringSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaturalprogrammerSpringSampleApplication.class, args);
+    }
+}
