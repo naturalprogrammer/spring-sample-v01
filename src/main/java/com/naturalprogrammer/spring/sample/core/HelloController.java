13c5463076889aafbd3bcd3e5f9f336bfb3bba14
Sanjay <skpatel20@gmail.com>
2015-01-09 12:58:55 +0530

    Upto 1100-application-properties

--- src/main/java/com/naturalprogrammer/spring/sample/core/HelloController.java
+++ src/main/java/com/naturalprogrammer/spring/sample/core/HelloController.java
@@ -1,14 +1,18 @@
 package com.naturalprogrammer.spring.sample.core;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
+	@Value("${app.name}")
+	private String appName;
+	
 	@RequestMapping("/hello")
 	public String hello() {
-		return "Hello, world";
+		return "Hello from " + appName;
 	}
 
 }
